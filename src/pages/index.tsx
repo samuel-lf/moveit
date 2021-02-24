0943cbdc5541745b65192a864507bcb007bcb7b1
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-24 20:32:34 -0300

    Adição do profile e desafios completos na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { CompletedChallenges } from '../components/CompletedChallenge';
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
@@ -7,6 +9,15 @@ export default function Home() {
     <>
       <div className={styles.container}>
         <ExperienceBar />
+
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+          </div>
+          <div>
+          </div>
+        </section>
       </div>
     </>
   )

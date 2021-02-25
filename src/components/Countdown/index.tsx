38b2440542f379d3213232935fb9dc53f42e8766
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-25 02:20:39 -0300

    Refatoração do Countdown com valores dinamicos

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengesContext';
 import styles from './Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout
@@ -7,6 +8,8 @@ export function Countdown() {
   const [time, setTime] = useState(0.1*60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
+  const { startNewChallenge } = useContext(ChallengesContext);
+  
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -32,6 +35,7 @@ export function Countdown() {
     } else if (isActive && time === 0){
       setHasFinished(true);
       setIsActive(false);
+      startNewChallenge();
     }
   }, [isActive, time])
 

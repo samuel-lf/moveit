b48f962b29c309910e43fd950c49ac6d0a6ab090
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-26 09:48:09 -0300

    Adição dos dados do perfil nos cookies

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 import challenges from '../../challenges.json'
+import Cookies from 'js-cookie'
 
 interface ChallengesProviderProps {
   children: ReactNode;
@@ -35,6 +36,12 @@ export const ChallengesProvider = ({ children }: ChallengesProviderProps ) => {
   useEffect(() => {
     Notification.requestPermission()
   }, [])
+
+  useEffect(() => {
+    Cookies.set('level', String(level))
+    Cookies.set('currentExperience', String(currentExperience))
+    Cookies.set('challengesCompleted', String(challengesCompleted))
+  }, [level, currentExperience, challengesCompleted])
 
   function levelUp() {
     setLevel(level + 1)
